6dbbb7cb7abc40f8fc7d3a68c5cb29a6fc45e1b8
mieum12 <qpdlqltb1215@gmail.com>
2024-01-25 15:35:31 +0900

    refactor: firebase loading screen 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import LandingPage from "./routes/LandingPage";
@@ -8,6 +8,9 @@ import JoinPage from "./routes/JoinPage";
 import MyPage from "./routes/MyPage";
 import RootLayout from "./routes/RootLayout";
 import ShopPage from "./routes/ShopPage";
+import {auth} from "./firebase";
+import LoadingScreen from "./components/LoadingScreen";
+import styled from "styled-components";
 
 const router = createBrowserRouter([
   {
@@ -28,8 +31,26 @@ const router = createBrowserRouter([
   },
 ]);
 
-function App() {
-  return <RouterProvider router={router} />;
+export default function App() {
+  const [isLoading, setIsLoading] = useState(true)
+  const init = async ()=>{
+    // firebase가 로그인 여부나 유저가 누구인지 확인할 떄까지 기다리는 부분
+    await auth.authStateReady()
+    // firebase가 로딩이 완료되면
+    setIsLoading(false)
+
+  }
+
+  useEffect(()=>{
+    init()
+  },[])
+  return <Wrapper>
+    {isLoading ? <LoadingScreen/> : <RouterProvider router={router}/>}
+  </Wrapper>
 }
 
-export default App;
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`
